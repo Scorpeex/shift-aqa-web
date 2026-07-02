d9b7d0ab0add991c9beccba5d1d99d887d0dc733
Scorpeex <argentum.nrj@gmail.com>
2026-07-02 10:21:44 +0700

    Homework 1

---
+++ tests/homeWorkOne.spec.ts
@@ -0,0 +1,99 @@
+import {test, expect} from '@playwright/test'
+import config from '../playwright.config'
+
+const baseURL = config.use?.baseURL
+
+if (!baseURL) {
+    throw new Error('baseURL не задан в конфиге')
+}
+
+test('Проверка ссылки в логотипе (Хедер) переход по ссылке', async ({page})=> {
+    await page.goto('catalog')
+    await page.getByTestId('header-logo').click()
+    await expect.soft(page).toHaveTitle('Главная')
+    await expect.soft(page).toHaveURL(baseURL)    
+})
+
+test('Проверка перехода в корзину из хедера', async ({page})=> {
+    await page.goto('')
+    await page.getByTestId('header-cart-button').click()
+    await expect.soft(page.getByTestId('cart-title')).toHaveText('Корзина')
+    await expect.soft(page).toHaveURL(`${baseURL}cart`)    
+})
+
+test('Проверка перехода в каталог из хедера', async ({page})=> {
+    await page.goto('')
+    await page.getByTestId('header-nav-link-catalog').click()
+    await expect.soft(page.getByTestId('catalog-title')).toHaveText('Каталог сладостей')
+    await expect.soft(page).toHaveURL(`${baseURL}catalog`)        
+})
+
+test('Проверка перехода в акции из хедера', async ({page})=> {
+    await page.goto('')
+    await page.getByTestId('header-nav-link-promotions').click()
+    await expect.soft(page.getByTestId('promotions-title')).toHaveText('Акции и специальные предложения')
+    await expect.soft(page).toHaveURL(`${baseURL}promotions`)        
+})
+
+test('Проверка перехода в доставку из хедера', async ({page})=> {
+    await page.goto('')
+    await page.getByTestId('header-nav-link-delivery').click()
+    await expect.soft(page.getByTestId('delivery-title')).toHaveText('Доставка и оплата')
+    await expect.soft(page).toHaveURL(`${baseURL}delivery`)        
+})
+
+test('Проверка перехода в раздел О нас из хедера', async ({page})=> {
+    await page.goto('')
+    await page.getByTestId('header-nav-link-about').click()
+    await expect.soft(page.getByTestId('about-title')).toHaveText('О компании')
+    await expect.soft(page).toHaveURL(`${baseURL}about`)        
+})
+
+test('Проверка перехода в контакты из хедера', async ({page})=> {
+    await page.goto('')
+    await page.getByTestId('header-nav-link-contacts').click()
+    await expect.soft(page.getByTestId('contacts-title')).toHaveText('Контакты')
+    await expect.soft(page).toHaveURL(`${baseURL}contacts`)        
+})
+
+test('Проверка перехода в обратную связь из хедера', async ({page})=> {
+    await page.goto('')
+    await page.getByTestId('header-nav-link-feedback').click()
+    await expect.soft(page.getByTestId('feedback-title')).toHaveText('Обратная связь')
+    await expect.soft(page).toHaveURL(`${baseURL}feedback`)        
+})
+
+test('Проверка перехода в FAQ из хедера', async ({page})=> {
+    await page.goto('')
+    await page.getByTestId('header-nav-link-faq').click()
+    await expect.soft(page.getByTestId('faq-title')).toHaveText('Часто задаваемые вопросы')
+    await expect.soft(page).toHaveURL(`${baseURL}faq`)        
+})
+
+test('Проверка перехода в Политика конфиденциальности из плашки кук', async ({page})=> {
+    await page.goto('')
+    await expect.soft(page.getByTestId('cookie-consent-banner')).toBeVisible()
+    await page.getByTestId('cookie-consent-privacy-link').click()
+    await expect.soft(page.getByTestId('privacy-title')).toHaveText('Политика конфиденциальности')
+    await expect.soft(page).toHaveURL(`${baseURL}privacy`)        
+})
+
+test('Проверка закрытия плашки кук при отказе от кук', async ({page})=> {
+    await page.goto('')
+    await expect.soft(page.getByTestId('cookie-consent-banner')).toBeVisible()
+    await page.getByTestId('cookie-decline-button').click()
+    await expect.soft(page.getByTestId('cookie-consent-banner')).toBeHidden()    
+})
+
+test('Проверка закрытия плашки кук при соглашении с куками', async ({page})=> {
+    await page.goto('')
+    await expect.soft(page.getByTestId('cookie-consent-banner')).toBeVisible()
+    await page.getByTestId('cookie-accept-button').click()
+    await expect.soft(page.getByTestId('cookie-consent-banner')).toBeHidden()    
+})
+
+//      ОСОБЕННОСТИ СТРАНИЦЫ КАТАЛОГ
+//  1.Присутствует пагинация, которая не меняет урл
+//  2.Есть фильтры, которые так же не меняют урл
+
+//  Обе особенности потенциально усложняют процесс написания автотестов
